d9d411e730d2a720478c75e8852d423ddd447360
liuwei <i@liuwei.co>
2013-05-31 10:33:53 +0800

    test git

--- src/com/myc/Appstart.java
+++ src/com/myc/Appstart.java
@@ -10,6 +10,7 @@ public class Appstart extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_appstart);
+		
 	}
 
 	@Override
